90b8532d86f3c34982c301bcd5b74608fcc476ad
luisx <luisxavier21@hotmail.com>
2023-08-15 00:44:51 -0600

    First commit. Added the first code

---
+++ 2 Big O/1-BigO intro.py
@@ -0,0 +1,8 @@
+# We will create a function to call print items
+# Pass a number _n_ and we will iterate _n_ times and will print i
+def print_items(n):
+    for i in range(n):
+        print(i)
+
+# Test
+print_items(10)
