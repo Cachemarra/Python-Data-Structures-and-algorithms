104d31ab4ee4dfa9c753ccbedc17b173480b42c1
luisx <luisxavier21@hotmail.com>
2023-08-17 00:01:02 -0600

    Big O notation classes added

--- 2 Big O/1-BigO intro.py
+++ 2 Big O/1-BigO intro.py
@@ -1,6 +1,9 @@
 # We will create a function to call print items
-# Pass a number _n_ and we will iterate _n_ times and will print i
+# Pass a number _n_ and we will iterate _n_ times 
+# and will print i
+
 def print_items(n):
+    # This is an O(n) complex problem.
     for i in range(n):
         print(i)
 
---
+++ 2 Big O/3-O_square.py
@@ -0,0 +1,8 @@
+#%% Basic O square problem
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            print(i, j)
+
+
+print_items(10)
