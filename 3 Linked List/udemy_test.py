47e9e007f831ce7f36201a405896c9533ae32295
luisx <luisxavier21@hotmail.com>
2023-09-05 00:14:36 -0600

    Added new method Working in "reverse_between"

--- 3 Linked List/udemy_test.py
+++ 3 Linked List/udemy_test.py
@@ -9,24 +9,6 @@ class LinkedList:
         new_node = Node(value)
         self.head = new_node
         self.length = 1
-        
-    def print_list(self):
-        temp = self.head
-        while temp is not None:
-            print(temp.value)
-            temp = temp.next  
-            
-    def print_all(self):
-        if self.length == 0:
-            print("Head: None")
-        else:
-            print("Head: ", self.head.value)
-        print("Length: ", self.length)
-        print("\nLinked List:")
-        if self.length == 0:
-            print("empty")
-        else:
-            self.print_list()
 
     def append(self, value):
         new_node = Node(value)
@@ -38,67 +20,111 @@ class LinkedList:
                 current = current.next
             current.next = new_node
         self.length += 1
+        return True
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next    
+            
+    def make_empty(self):
+        self.head = None
+        self.length = 0
 
+    # WRITE REVERSE_BETWEEN METHOD HERE #
+    #                                   #
+    #                                   #
+    #                                   #
+    #                                   #
+    #####################################
+    def reverse_between(self, m, n):
+        temp = self.head
+        sub_node = self.head
 
-    # WRITE REMOVE_DUPLICATES METHOD HERE #
-    #                                     #
-    #                                     #
-    #                                     #
-    #                                     #
-    #######################################
-    def remove_duplicates(self):
-        if self.length == 0:
+        if temp == None or m == 0 or n == 0:
             return None
         
-        # Extract all node values
-        temp = self.head
-        node_values = []
-        while temp != None:
-            node_values.append(temp.value)
+        for i in range(m):
+            sub_node = sub_node.next
             temp = temp.next
+        wait = sub_node
+
+        i = 1
+        while new != None and i != n:
+            new = sub_node.next
+            temp = new
+            temp.next = sub_node
+            sub_node = sub_node.next
+            i += 1
         
-        node_values = set(node_values)
-        node_values.remove(self.head.value)
-        temp = self.head
-        next_temp = self.head.next
+        temp.next = wait
+        temp = temp.next
+        temp.next = None
+        
 
-        while next_temp != None:
-            if next_temp.value in node_values:
-                node_values.remove(next_temp.value)
+# %% 
+linked_list = LinkedList(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.append(4)
+linked_list.append(5)
 
-                temp.next = next_temp
-                temp = temp.next
+print("Original linked list: ")
+linked_list.print_list()
 
-            next_temp = next_temp.next
-                
+# Reverse a sublist within the linked list
+linked_list.reverse_between(2, 4)
+print("Reversed sublist (2, 4): ")
+linked_list.print_list()
 
+# Reverse another sublist within the linked list
+linked_list.reverse_between(0, 4)
+print("Reversed entire linked list: ")
+linked_list.print_list()
 
+# Reverse a sublist of length 1 within the linked list
+linked_list.reverse_between(3, 3)
+print("Reversed sublist of length 1 (3, 3): ")
+linked_list.print_list()
 
-# %%
-my_linked_list = LinkedList(1)
-my_linked_list.append(1)
-my_linked_list.append(2)
-my_linked_list.append(3)
-my_linked_list.append(3)
-my_linked_list.append(2)
-my_linked_list.append(4)
-my_linked_list.remove_duplicates()
-
-my_linked_list.print_all()
-
-
+# Reverse an empty linked list
+empty_list = LinkedList(0)
+empty_list.make_empty
+empty_list.reverse_between(0, 0)
+print("Reversed empty linked list: ")
+empty_list.print_list()
 
 
 """
     EXPECTED OUTPUT:
     ----------------
-    Head:  1
-    Length:  4
-    Linked List:
+    Original linked list: 
     1
     2
     3
     4
+    5
+    Reversed sublist (2, 4): 
+    1
+    2
+    5
+    4
+    3
+    Reversed entire linked list: 
+    3
+    4
+    5
+    2
+    1
+    Reversed sublist of length 1 (3, 3): 
+    3
+    4
+    5
+    2
+    1
+    Reversed empty linked list: 
+    None
     
 """
 
