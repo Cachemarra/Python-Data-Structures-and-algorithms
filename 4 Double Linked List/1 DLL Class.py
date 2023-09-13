f55faba0b9f9ac16f16c85767864d20150f019b4
luisx <luisxavier21@hotmail.com>
2023-09-12 22:19:35 -0600

    Added methods for Double Linked List

--- 4 Double Linked List/1 DLL Class.py
+++ 4 Double Linked List/1 DLL Class.py
@@ -36,9 +36,84 @@ class DoubleLinkedList:
         self.__update_len(self.INCREASE_LEN)
         return True
 
+    def prepend(self, value):
+        new_node = Node(value)
+
+        temp = self.head
+        new_node.next = temp
+
+        if self.length == 0:
+            self.tail = new_node
+        else:
+            temp.prev = new_node
+
+        self.head = new_node
+        return True
+        
+
+    def insert(self, index, value):
+        pass
+
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        
+        # Storing actual tail
+        temp = self.tail
+
+        # Update tail
+        new_tail = temp.prev
+        new_tail.next = None
+        self.tail = new_tail
+
+        temp.prev = None
+        self.__update_len(self.DECREACE_LEN)
+        return temp
+
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        
+        temp = self.head
+        new_head = temp.next
+        new_head.prev = None
+        self.head = new_head
+
+        temp.prev = None
+        self.__update_len(self.DECREACE_LEN)
+        return temp
+
+    
+    def get(self, index):
+        if index > self.length or index < 0:
+            return False
+        
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+
+        return temp
+        
+
+    def set(self, index, value):
+        pass
+
+
+    def remove(self, index):
+        pass
+
+
+    def reverse(self):
+        pass
 
 
 
+
+
+
+    # Dunder methods ========
     def __str__(self):
         text = "None"
         temp = self.head
