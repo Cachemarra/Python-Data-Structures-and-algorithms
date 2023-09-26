8984369a68774735af79672dd48c9be8c4218bfa
luisx <luisxavier21@hotmail.com>
2023-09-25 23:43:06 -0600

    Finish base Stack

--- 5 Stacks and Queues/1 Stack.py
+++ 5 Stacks and Queues/1 Stack.py
@@ -6,6 +6,79 @@ class Node():
 
 
 # %% Stack constructor
+class Stack:
+    __INCREASE_HEIGHT = 1
+    __DECREASE_HEIGHT = -1
+
+    def __init__(self, value=None):
+        node = Node(value)
+
+        self.top = node
+        self.height = 1
 
 
+    def push(self, value):
+        node = Node(value)
 
+        temp = self.top
+        if temp.value is None and self.height == 1:
+            self.top = node
+            return True
+        else:
+            node.next = temp
+            self.top = node
+
+            self.__updateHeight__(self.__INCREASE_HEIGHT)
+            return True
+        
+
+    def pop(self):
+        
+        if self.height >= 1:
+            temp = self.top
+            self.top = temp.next
+            temp.next = None
+
+            self.__updateHeight__(self.__DECREASE_HEIGHT)
+            return temp
+        return None
+
+
+    def __str__(self):
+        text = ""
+        
+        temp = self.top
+        while temp is not None:
+            value = temp.value
+            text += f"{value}\n↓\n"
+            temp = temp.next
+
+        text += "None"
+        return text
+
+    def __updateHeight__(self, value):
+        self.height += value
+
+
+# %% Check
+
+my_stack = Stack(4)
+print(my_stack)
+
+print("".center(30, "-"))
+
+my_stack.push(11)
+my_stack.push(5)
+my_stack.push(0)
+my_stack.push(1)
+my_stack.push(3)
+print(my_stack)
+
+print("".center(30, "-"))
+
+print(f"Node value: {my_stack.pop().value}")
+print(f"Node value: {my_stack.pop().value}")
+print("Final stack")
+print(my_stack)
+
+# %%
--- 5 Stacks and Queues/2 Queue.py
+++ 5 Stacks and Queues/2 Queue.py
@@ -6,4 +6,18 @@ class Node():
 
 
 # %% Queue constructor
+class Queue():
+    self.__INCREASE_LEN = 1
+    self.__DECREASE_LEN = -1
+
+
+    def __init__(self, value:None):
+        node = Node(value)
+
+        self.first = node
+        self.last = node
+        self.length = 1
+
+
+    def
 
