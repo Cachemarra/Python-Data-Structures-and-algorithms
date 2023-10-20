96b5a1f445e893d50c9a99ad84bce18e29e19ead
luisx <luisxavier21@hotmail.com>
2023-10-19 23:57:18 -0600

    Finished Two sum

---
+++ 7 Hash table/two_sum.py
@@ -0,0 +1,40 @@
+
+def two_sum(nums, target):
+    for index, num in enumerate(nums):
+        nums[index] = None
+        
+        num_target = abs(num-target)
+        
+        if num_target in nums:
+            num_target_idx = nums.index(num_target)
+            return [index, num_target_idx]
+    return []
+    
+    
+    
+print(two_sum([5, 1, 7, 2, 9, 3], 10))  
+print(two_sum([4, 2, 11, 7, 6, 3], 9))  
+print(two_sum([10, 15, 5, 2, 8, 1, 7], 12))  
+print(two_sum([1, 3, 5, 7, 9], 10))  
+print ( two_sum([1, 2, 3, 4, 5], 10) )
+print ( two_sum([1, 2, 3, 4, 5], 7) )
+print ( two_sum([1, 2, 3, 4, 5], 3) )
+print ( two_sum([], 0) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 4]
+    [1, 3]
+    [0, 3]
+    [1, 3]
+    []
+    [2, 3]
+    [0, 1]
+    []
+
+"""
+
+
